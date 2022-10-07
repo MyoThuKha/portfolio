3cceb68f3b7c37747342f799f06e2b83dc7e7702
MyoThuKha <myothukha@outlook.sg>
2022-10-07 21:27:53 +0630

    change contact ui

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Contact.module.css";
 import { motion } from "framer-motion";
 import { linkVariant } from "../animation/contactVariant";
+import Link from "next/link";
 
 const Contact = () => {
   return (
@@ -8,50 +9,60 @@ const Contact = () => {
       <div className="titleHeader">
         <div className={styles.pageTitle}>Contact&nbsp;Me</div>
       </div>
+
       <div className="row">
-        <h5 className="mt-5">Social</h5>
-        <div className="d-flex align-items-center">
-          <div className=" d-inline-block">
-            <SocialLink
-              name="Facebook"
-              url="https://www.facebook.com/myo.tha.79"
-            />
-            <SocialLink
-              name="Twitter"
-              url="https://twitter.com/iam_myothukha"
-            />
-            <SocialLink
-              name="LinkedIn"
-              url="https://www.linkedin.com/in/myo-thu-kha-170077199"
-            />
+        <section className="col justify-content-center">
+          <h4>Social</h4>
+          <div className="d-flex">
+            <SocialLink url="https://www.facebook.com/myo.tha.79">
+              Facebook
+            </SocialLink>
           </div>
-        </div>
-
-        {/*  */}
-        <div>
-          <h5 className="mt-5">Gmail</h5>
-          <div>myothukha.dev@gmail.com</div>
-        </div>
-
-        {/*  */}
-        <h5 className="mt-5">Github</h5>
-        <SocialLink
-          name="Myo Thu Kha"
-          url="https://www.github.com/MyoThuKha
-"
-        />
+          <div className="d-flex">
+            <SocialLink url="https://twitter.com/iam_myothukha">
+              Twitter
+            </SocialLink>
+          </div>
+          <div className="d-flex">
+            <SocialLink url="https://www.linkedin.com/in/myo-thu-kha-170077199">
+              LinkedIn
+            </SocialLink>
+          </div>
+          
+        </section>
+        <section
+          style={{ height: "50vh" }}
+          className="col border border-2 border-dark"
+        >
+          <div className={styles.hello}>Hello</div>
+          <h3>To</h3>
+          <a href="mailto:myothukha.dev@gmail.com" className={styles.link}>
+            myothukha.dev@gmail.com
+          </a>
+          <hr />
+          <hr />
+          <hr />
+          <hr />
+          <div></div>
+        </section>
       </div>
     </div>
   );
 };
 
-const SocialLink = ({ name, url }) => {
+const SocialLink = ({ url, children }) => {
   return (
-    <motion.div variants={linkVariant} whileHover="hover">
-      <a className={styles.link} href={url} target="_blank" rel="noreferrer">
-        {name}
-      </a>
-      <i className="bi bi-box-arrow-up-right"></i>
+    <motion.div variants={linkVariant} whileHover="hover" className="d-flex">
+      <div className={styles.link}>
+        <Link href={url}>
+          <a className={styles.link} target="_blank" rel="noreferrer">
+            {children}
+          </a>
+        </Link>
+      </div>
+      <motion.div>
+        <i className="bi bi-box-arrow-up-right"></i>
+      </motion.div>
     </motion.div>
   );
 };
