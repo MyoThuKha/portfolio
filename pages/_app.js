7b94125014160e6983e04cf2306694f2b4847d6c
MyoThuKha <myothukha@outlook.sg>
2022-11-19 19:15:12 +0630

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,9 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.css";
 function MyApp({ Component, pageProps }) {
   return (
-    <div style={{ backgroundColor: "#eeeff1" }}>
+    <div className="bodyBox">
       <NavBar></NavBar>
-      <div className="ps-4 pe-3 bodyBox">
+      <div className="ps-4 pe-3">
         <Component {...pageProps} />
       </div>
     </div>
